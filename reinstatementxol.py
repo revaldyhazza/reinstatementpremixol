d8518900e7a8a81972880bc7e8ef7c5d3605290d
Revaldy Hazza Daniswara <diohazza123@gmail.com>
2025-07-07 10:19:45 +0700

    Update reinstatementxol.py

--- reinstatementxol.py
+++ reinstatementxol.py
@@ -299,7 +299,7 @@ def ringkasan_data_asli(df_soc_real, ur, layer):
     for i in range(1, 7):
         layer_data = df_soc_real[f'Layer {i}'].dropna()
         total_klaim = int(layer_data.sum()) if not layer_data.empty else 0
-        frekuensi_klaim = int(len(layer_data[layer_data > 0]))
+        fre Officials_klaim = int(len(layer_data[layer_data > 0]))
         rata_rata_klaim = int(total_klaim / frekuensi_klaim) if frekuensi_klaim > 0 else 0
         
         summary_data.append({
@@ -405,7 +405,7 @@ if file_severitas and file_frekuensi:
     param_poisson = {'mu': rata_rata_frekuensi}
     param_negbinom = {
         'p': rata_rata_frekuensi / varians_frekuensi if varians_frekuensi > rata_rata_frekuensi else 0.99,
-        'n': rata_rata_frekuensi 2 / (varians_frekuensi - rata_rata_frekuensi) if varians_frekuensi > rata_rata_frekuensi else rata_rata_frekuensi
+        'n': rata_rata_frekuensi ** 2 / (varians_frekuensi - rata_rata_frekuensi) if varians_frekuensi > rata_rata_frekuensi else rata_rata_frekuensi
     }
     param_geom = {'p': 1 / rata_rata_frekuensi if rata_rata_frekuensi > 0 else 0.99}
     
